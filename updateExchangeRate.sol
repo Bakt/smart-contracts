0167ce361a56c1d18ec2ace3a5efd357e4fb376a
CyberNano <cybernano16@gmail.com>
2017-01-19 17:57:27 +1300

    Delete updateExchangeRate.sol

--- updateExchangeRate.sol
+++
@@ -1,5 +0,0 @@
-    function updateExchangeRate() payable {
-        var startBalance = this.balance;
-        oraclize_query("URL", "json(http://api.etherscan.io/api?module=stats&action=ethprice).result.ethusd", 300000);
-        if (startBalance - this.balance > msg.value) throw;
-    }
