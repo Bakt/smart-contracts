403a3224034f6a5f7e3f56167ecfca8be2add37d
g. nicholas d'andrea <gnidan@dramafever.com>
2017-01-26 22:28:29 -0500

    Initial truffle setup

---
+++ test/TestBackedValueContract.js
@@ -0,0 +1,8 @@
+require('mocha-generators').install();
+
+var { fetchEvent } = require('./helpers');
+
+contract('BackedValueContract', function(accounts) {
+    it("should be deployable", function* () {
+    });
+});
