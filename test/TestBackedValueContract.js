bc6559fc390c29640f3e55564e9372402d68e888
g. nicholas d'andrea <gnidan@dramafever.com>
2017-02-05 18:54:24 -0500

    Write test for deploying BVC

--- test/TestBackedValueContract.js
+++ test/TestBackedValueContract.js
@@ -2,7 +2,27 @@ require('mocha-generators').install();
 
 var { fetchEvent } = require('./helpers');
 
+var BackedValueContract = artifacts.require('BackedValueContract.sol');
+
 contract('BackedValueContract', function(accounts) {
+    let emitter = accounts[0];
+    let beneficiary = accounts[1];
+
     it("should be deployable", function* () {
+        // setup
+        var notionalValue = web3.toBigNumber('1000000');
+        // action
+        var bvc = yield BackedValueContract.new(
+            beneficiary, notionalValue, {from: emitter}
+        );
+
+        // checking result
+        var actualBeneficiary = yield bvc.beneficiary();
+        var actualEmitter = yield bvc.emitter();
+        var actualNotionalValue = yield bvc.notionalValue();
+
+        assert.equal(beneficiary.toString(), actualBeneficiary.toString());
+        assert.equal(emitter.toString(), actualEmitter.toString());
+        assert.equal(notionalValue.toString(), actualNotionalValue.toString());
     });
 });
