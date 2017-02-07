75b32eed156d742cf6429c16152b15bceab103ae
g. nicholas d'andrea <gnidan@dramafever.com>
2017-02-06 19:19:21 -0500

    Abstract service deployment into single function

--- migrations/4_deploy_exchange_rate.js
+++ migrations/4_deploy_exchange_rate.js
@@ -1,30 +1,7 @@
-var Services = artifacts.require("Services.sol");
-var ExchangeRate = artifacts.require("ExchangeRate.sol");
+let ExchangeRate = artifacts.require("ExchangeRate.sol");
+
+let { deployService } = require('../lib/deployment');
 
 module.exports = function(deployer) {
-    var services;
-    return deployer
-        .then(function() {
-            return Services.deployed();
-        })
-        .then(function (instance) {
-            services = instance;
-            console.log(
-                "Deploying ExchangeRate with services address ", services.address
-            );
-            return deployer.deploy(ExchangeRate, services.address);
-        })
-        .then(function () {
-            return ExchangeRate.deployed();
-        })
-        .then(function (instance) {
-            console.log("Deployed ExchangeRate with address ", instance.address);
-            console.log("Specifying ExchangeRate service");
-            return services.specifyService(
-                web3.sha3("ExchangeRate"), instance.address
-            );
-        }).then(function () {
-            console.log("Service specified");
-        });
+    return deployService(web3, deployer, artifacts, ExchangeRate);
 };
-
