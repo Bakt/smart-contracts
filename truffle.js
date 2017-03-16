fee4c348dbc5fe33cdaeffc8cf77d968668c514d
Chris Hatch <hatch@tuta.io>
2017-03-16 09:09:37 +0530

    oops - restore hostname based hosts in truffle.js

--- truffle.js
+++ truffle.js
@@ -1,12 +1,12 @@
 module.exports = {
     networks: {
         "development": {
-            host: "172.19.0.2",
+            host: "testrpc",
             port: 8545,
             network_id: "*"
         },
         "testnet": {
-            host: "52.221.123.6",
+            host: "parity-testnet",
             port: 8545,
             network_id: 42,
             gas: 4000000
